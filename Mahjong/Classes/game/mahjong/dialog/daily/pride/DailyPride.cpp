5fa2e751ee81308fa035674d8aa5914249f0d704
qz515880 <1195933889@qq.com>
2016-06-12 13:48:05 +0800

    上海麻将

--- Mahjong/Classes/game/mahjong/dialog/daily/pride/DailyPride.cpp
+++ Mahjong/Classes/game/mahjong/dialog/daily/pride/DailyPride.cpp
@@ -6,17 +6,17 @@
 #include "userdata/UserData.h"
 #include "math.h"
 bool DailyPride::init(){
-	if (!Layer::init()){
-		return false;
-	}
-	showDailyPrideLayer();
-	return true;
-
+    if (!Layer::init()){
+        return false;
+    }
+    showDailyPrideLayer();
+    return true;
+    
 }
 
 void DailyPride::onEnter(){
-	Layer::onEnter();
-	
+    Layer::onEnter();
+    
     prideCallBackListener1 = EventListenerCustom::create(MSG_PLAYER_DAILY_PRIDE, [=](EventCustom* event){
         updateData();
     });
@@ -32,7 +32,7 @@ void DailyPride::onEnter(){
                     startMenu->setEnabled(true);
                 }
             }
-
+            
         },0.0f,0,4.0f,"m_turnBg");
         
     });
@@ -40,67 +40,67 @@ void DailyPride::onEnter(){
 }
 
 void DailyPride::onExit(){
-	Layer::onExit();
-	_eventDispatcher->removeEventListener(prideCallBackListener1);
+    Layer::onExit();
+    _eventDispatcher->removeEventListener(prideCallBackListener1);
     _eventDispatcher->removeEventListener(prideCallBackListener2);
-
+    
 }
 
 
 void DailyPride::showDailyPrideLayer(){
-	auto text_bg = Sprite::create("daily/pride/text_info_bg.png");
-	text_bg->setPosition(850, 370);
-	addChild(text_bg);
-
-	auto piao = Sprite::create("daily/pride/piao_dai.png");
-	piao->setPosition(900, 480);
-	addChild(piao);
-
-	auto title = Sprite::create("daily/pride/god_text.png");
-	title->setPosition(900, 500);
-	addChild(title);
-
-	auto hint = Sprite::create("daily/pride/chance_text.png");
-	hint->setPosition(900, 420);
-	addChild(hint);
-
+    auto text_bg = Sprite::create("daily/pride/text_info_bg.png");
+    text_bg->setPosition(850, 370);
+    addChild(text_bg);
+    
+    auto piao = Sprite::create("daily/pride/piao_dai.png");
+    piao->setPosition(900, 480);
+    addChild(piao);
+    
+    auto title = Sprite::create("daily/pride/god_text.png");
+    title->setPosition(900, 500);
+    addChild(title);
+    
+    auto hint = Sprite::create("daily/pride/chance_text.png");
+    hint->setPosition(900, 420);
+    addChild(hint);
+    
     auto num = Sprite::create(getImageNameById(atoi(GAMEDATA::getInstance()->getDailyPrideData().count.c_str())));
-	num->setPosition(910,425);
+    num->setPosition(910,425);
     num->setTag(1000);
-	addChild(num);
-
-	auto text = Sprite::create("daily/pride/text_info.png");
-	text->setPosition(890, 320);
-	addChild(text);
-
-	auto luck_bg = Sprite::create("daily/pride/luck_bg.png");
-	luck_bg->setPosition(463, 325);
-	addChild(luck_bg);
-
-	m_turnBg = Sprite::create("daily/pride/circle_bg.png");
-	m_turnBg->setPosition(463, 325);
-	addChild(m_turnBg);
-
+    addChild(num);
+    
+    auto text = Sprite::create("daily/pride/text_info.png");
+    text->setPosition(890, 320);
+    addChild(text);
+    
+    auto luck_bg = Sprite::create("daily/pride/luck_bg.png");
+    luck_bg->setPosition(463, 325);
+    addChild(luck_bg);
+    
+    m_turnBg = Sprite::create("daily/pride/circle_bg.png");
+    m_turnBg->setPosition(463, 325);
+    addChild(m_turnBg);
+    
     DailyPrideData data = GAMEDATA::getInstance()->getDailyPrideData();
-	for (int i = 0; i < data.prides.size(); i++){
+    for (int i = 0; i < data.prides.size(); i++){
         PrideCell* cell = PrideCell::create(data.prides.at(i).type, data.prides.at(i).number);
-		cell->setRotation(90-i*45);
+        cell->setRotation(90-i*45);
         cell->setTag(100+i);
-		cell->setPosition(getPosByRotation(Point(219,219), 100, i * 45));
-		m_turnBg->addChild(cell);
-	}
-
-	auto arrows = Sprite::create("daily/pride/arrows.png");
-	arrows->setPosition(463, 335);
-	addChild(arrows);
-
-	auto itemImage = MenuItemImage::create("daily/pride/go_btn_1.png", "daily/pride/go_btn_2.png",
-		CC_CALLBACK_0(DailyPride::beginPride, this));
-	startMenu = Menu::create(itemImage, NULL);
-	startMenu->setPosition(463, 325);
-	addChild(startMenu);
-
-
+        cell->setPosition(getPosByRotation(Point(219,219), 100, i * 45));
+        m_turnBg->addChild(cell);
+    }
+    
+    auto arrows = Sprite::create("daily/pride/arrows.png");
+    arrows->setPosition(463, 335);
+    addChild(arrows);
+    
+    auto itemImage = MenuItemImage::create("daily/pride/go_btn_1.png", "daily/pride/go_btn_2.png",
+                                           CC_CALLBACK_0(DailyPride::beginPride, this));
+    startMenu = Menu::create(itemImage, NULL);
+    startMenu->setPosition(463, 325);
+    addChild(startMenu);
+    
+    
 }
 
 void DailyPride::updateData(){
@@ -117,11 +117,12 @@ void DailyPride::updateData(){
 
 
 void DailyPride::beginPride(){
-    if(UserData::getInstance()->getGold()<50000){
-        HintDialog* hit = HintDialog::create("金币不足,请充值",false);
-        addChild(hit);
-    }else{
-        if(atoi(GAMEDATA::getInstance()->getDailyPrideData().count.c_str())>0){
+    
+    if(atoi(GAMEDATA::getInstance()->getDailyPrideData().count.c_str())>0){
+        if(UserData::getInstance()->getGold()<50000){
+            HintDialog* hit = HintDialog::create("未达成抽奖条件",false);
+            addChild(hit);
+        }else{
             NetworkManage::getInstance()->sendMsg(CommandManage::getInstance()->getTodayPrideCommand());
             startMenu->setEnabled(false);
             srand(unsigned(time(NULL)));
@@ -132,66 +133,69 @@ void DailyPride::beginPride(){
             auto sequence = Sequence::create(pAction, repeat,NULL);
             m_turnBg->runAction(sequence);
         }
+    }else{
+        HintDialog* hit = HintDialog::create("金币不足,请充值",false);
+        addChild(hit);
     }
 }
 
 std::string DailyPride::getImageNameById(int id){
-	std::string dayImage = "bignum/zero_1.png";
-	switch (id)
-	{
-	case 0:
-		dayImage = "bignum/zero_1.png";
-
-		break;
-	case 1:
-
-		dayImage = "bignum/one_1.png";
-
-		break;
-	case 2:
-
-		dayImage = "bignum/two_1.png";
-
-		break;
-	case 3:
-
-		dayImage = "bignum/three_1.png";
-
-		break;
-	case 4:
-
-		dayImage = "bignum/four_1.png";
-
-		break;
-	case 5:
-
-		dayImage = "bignum/five_1.png";
-
-		break;
-	case 6:
-
-		dayImage = "bignum/six_1.png";
-
-		break;
-	case 7:
-
-		dayImage = "bignum/seven_1.png";
-
-		break;
-	case 8:
-
-		dayImage = "bignum/eight_1.png";
-
-		break;
-	case 9:
-
-		dayImage = "bignum/nine_1.png";
-
-		break;
-	}
-	return dayImage;
+    std::string dayImage = "bignum/zero_1.png";
+    switch (id)
+    {
+        case 0:
+            dayImage = "bignum/zero_1.png";
+            
+            break;
+        case 1:
+            
+            dayImage = "bignum/one_1.png";
+            
+            break;
+        case 2:
+            
+            dayImage = "bignum/two_1.png";
+            
+            break;
+        case 3:
+            
+            dayImage = "bignum/three_1.png";
+            
+            break;
+        case 4:
+            
+            dayImage = "bignum/four_1.png";
+            
+            break;
+        case 5:
+            
+            dayImage = "bignum/five_1.png";
+            
+            break;
+        case 6:
+            
+            dayImage = "bignum/six_1.png";
+            
+            break;
+        case 7:
+            
+            dayImage = "bignum/seven_1.png";
+            
+            break;
+        case 8:
+            
+            dayImage = "bignum/eight_1.png";
+            
+            break;
+        case 9:
+            
+            dayImage = "bignum/nine_1.png";
+            
+            break;
+    }
+    return dayImage;
 }
 
 Point DailyPride::getPosByRotation(Point pos, float r, float a){
-	return Point(pos.x + cos(CC_DEGREES_TO_RADIANS(a))*r, pos.y + sin(CC_DEGREES_TO_RADIANS(a))*r);
+    return Point(pos.x + cos(CC_DEGREES_TO_RADIANS(a))*r, pos.y + sin(CC_DEGREES_TO_RADIANS(a))*r);
 }
