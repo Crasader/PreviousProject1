0a514fd4861eb877fab1c3fa70724a1c80f8e314
qz515880 <1195933889@qq.com>
2016-06-15 18:12:48 +0800

    上海麻将

--- Mahjong/Classes/image/UrlImageMannger.cpp
+++ Mahjong/Classes/image/UrlImageMannger.cpp
@@ -54,17 +54,5 @@ std::string UrlImageMannger::getImgNameByUrl(std::string url)
 
 
 void UrlImageMannger::uploadImage2Server(CallFunc* callBack){
-    //TODO
-    //    <form method="post" action="http://upload.qiniu.com/"
-    //    enctype="multipart/form-data">
-    //    <input name="key" type="hidden" value="<resource_key>">
-    //    <input name="x:<custom_name>" type="hidden" value="<custom_value>">
-    //    <input name="token" type="hidden" value="<upload_token>">
-    //    <input name="file" type="file" />
-    //    <input name="crc32" type="hidden" />
-    //    <input name="accept" type="hidden" />
-    //    </form>
-    
-
-    HttpMannger::getInstance()->httpToPostRequestToSendImage("http://up.qiniu.com", CommandManage::getInstance()->testQiniu());
+    //TODO 调用七牛SDK(android或者ios)
 }
