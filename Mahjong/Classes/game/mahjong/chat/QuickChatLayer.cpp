5fef41a5e7481da43f659a137db4db8d29031db0
qz515880 <1195933889@qq.com>
2016-08-17 11:18:32 +0800

    上海麻将

--- Mahjong/Classes/game/mahjong/chat/QuickChatLayer.cpp
+++ Mahjong/Classes/game/mahjong/chat/QuickChatLayer.cpp
@@ -23,7 +23,46 @@ bool QuickChatLayer::init(){
     quickChatBg->setPosition(Vec2(605,295));
     addChild(quickChatBg);
     
+    ListView* listView = ListView::create();
+    listView->setDirection(ui::ScrollView::Direction::VERTICAL);//设置ListView布局方向
+    listView->setTouchEnabled(true);//可触摸
+    listView->setContentSize(Size(620,360));//设置ListView大小
+    listView->setPosition(Vec2(610,295));
+    listView->setAnchorPoint(Point::ANCHOR_MIDDLE);
+    listView->addEventListener((ui::ListView::ccListViewCallback)CC_CALLBACK_2(QuickChatLayer::selectedItemEvent, this));
+    addChild(listView);
+    
+    for(int i=0; i<10;i++){
+        Button* chatbutton = Button::create();
+        chatbutton->setTitleText("quick chat index");
+        chatbutton->setTitleFontSize(35);
+        chatbutton->setTitleColor(Color3B(255,255,255));
+        listView->pushBackCustomItem(chatbutton);
+    }
+    
     return true;
+}
+
+void QuickChatLayer::selectedItemEvent(Ref *pSender, ListView::EventType type){
+    switch (type)
+    {
+        case cocos2d::ui::ListView::EventType::ON_SELECTED_ITEM_START:
+        {
+            ListView* listView = static_cast<ListView*>(pSender);
+            CC_UNUSED_PARAM(listView);
+            CCLOG("select child start index = %ld", listView->getCurSelectedIndex());
+            break;
+        }
+        case cocos2d::ui::ListView::EventType::ON_SELECTED_ITEM_END:
+        {
+            ListView* listView = static_cast<ListView*>(pSender);
+            CC_UNUSED_PARAM(listView);
+            CCLOG("select child end index = %ld", listView->getCurSelectedIndex());
+            break;
+        }
+        default:
+            break;
+    }
 }
 
 bool QuickChatLayer::onTouchBegan(Touch *touch, Event  *event){
@@ -31,7 +70,7 @@ bool QuickChatLayer::onTouchBegan(Touch *touch, Event  *event){
 }
 
 void QuickChatLayer:: onTouchMoved(Touch *touch, Event  *event){
-
+    
 }
 
 void QuickChatLayer:: onTouchEnded(Touch *touch, Event  *event){
@@ -39,5 +78,4 @@ void QuickChatLayer:: onTouchEnded(Touch *touch, Event  *event){
     if(!rect.containsPoint(touch->getLocation())){
         removeFromParent();
     }
-    
 }
--- Mahjong/Classes/game/mahjong/chat/QuickChatLayer.hpp
+++ Mahjong/Classes/game/mahjong/chat/QuickChatLayer.hpp
@@ -9,7 +9,10 @@
 #ifndef QuickChatLayer_hpp
 #define QuickChatLayer_hpp
 #include "cocos2d.h"
+#include "ui/UIListView.h"
+#include "ui/UIButton.h"
 USING_NS_CC;
+using namespace cocos2d::ui;
 
 class QuickChatLayer : public Layer{
 public:
@@ -19,5 +22,6 @@ private:
     bool onTouchBegan(Touch *touch, Event  *event) override;
     void onTouchMoved(Touch *touch, Event  *event) override;
     void onTouchEnded(Touch *touch, Event  *event) override;
+    void selectedItemEvent(Ref *pSender, ListView::EventType type);
 };
 #endif /* QuickChatLayer_hpp */
