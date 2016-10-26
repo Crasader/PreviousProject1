8f1f8460a3866fb0fe0bec5e9bfc05ecf7ff13c6
qz515880 <1195933889@qq.com>
2016-10-26 13:21:21 +0800

    上海麻将

--- Mahjong/Classes/game/mahjong/shop/ChargeGold.cpp
+++ Mahjong/Classes/game/mahjong/shop/ChargeGold.cpp
@@ -29,6 +29,7 @@ bool ChargeGold::init(){
     
     auto dialog_bg = Sprite::create("shop/charge_bg.png");
     dialog_bg->setPosition(640,360);
+    dialog_bg->setScaleX(1.2f);
     addChild(dialog_bg);
     
     auto title = Sprite::create("shop/buy_gold_title.png");
@@ -104,11 +105,11 @@ void ChargeGold::showChargeGold(){
     GoldChangeList list = GAMEDATA::getInstance()->getGoldChangeList();
     for(int i=0;i<list.list.size();i++){
         auto propBg =  Sprite::create("shop/prop_bg.png");
-        propBg->setPosition(285+175*i,345);
+        propBg->setPosition(205+175*i,345);
         addChild(propBg);
         
         auto light = Sprite::create("shop/gold_bg_light.png");
-        light->setPosition(285+175*i,370);
+        light->setPosition(205+175*i,370);
         addChild(light);
         
         auto diamond = Sprite::create();
@@ -120,11 +121,11 @@ void ChargeGold::showChargeGold(){
             diamond->setTexture("shop/gold_icon_3.png");
             
         }
-        diamond->setPosition(285+175*i,370);
+        diamond->setPosition(205+175*i,370);
         this->addChild(diamond);
         
         auto piao = Sprite::create();
-        piao->setPosition(285+175*i,280);
+        piao->setPosition(205+175*i,280);
         addChild(piao);
         
         if(i==0){
@@ -136,34 +137,36 @@ void ChargeGold::showChargeGold(){
             
         }
         
-        LabelAtlas* diamondNum = LabelAtlas::create(cocos2d::String::createWithFormat("%d",list.list.at(i).gold/10000)->_string,"shop/prop_num.png",21,28,'0');
-        diamondNum->setPosition(270+175*i,291);
+        LabelAtlas* diamondNum = LabelAtlas::create(StringUtils::format("%d",list.list.at(i).gold/10000),"shop/prop_num.png",21,28,'0');
+        diamondNum->setPosition(190+175*i,291);
         diamondNum->setAnchorPoint(Point::ANCHOR_MIDDLE_RIGHT);
         addChild(diamondNum);
         
         auto fontWan = Sprite::create("shop/font_wan.png");
         addChild(fontWan);
-        fontWan->setPosition(280+175*i,291);
+        fontWan->setPosition(200+175*i,291);
         
         auto zhuan = Sprite::create("shop/gold_text.png");
-        zhuan->setPosition(325+175*i,291);
+        zhuan->setPosition(245+175*i,291);
         addChild(zhuan);
         
         
         auto btnImage = MenuItemImage::create("shop/button_bg_1.png","shop/button_bg_2.png",CC_CALLBACK_1(ChargeGold::chargeGoldBtn, this));
         btnImage->setTag(list.list.at(i).gold);
         auto myMenu = Menu::create(btnImage,NULL);
-        myMenu->setPosition(285+175*i,225);
+        myMenu->setPosition(205+175*i,225);
         addChild(myMenu);
         
-        LabelAtlas* money = LabelAtlas::create(cocos2d::String::createWithFormat("%d",list.list.at(i).diamond)->_string,"shop/prop_num.png",21,28,'0');
-        money->setPosition(300+176*i,230);
+        
+        
+        auto yuan = Sprite::create("mjitem/fangka_icon.png");
+        yuan->setPosition(205+176*i,225);
+        addChild(yuan);
+        
+        LabelAtlas* money=LabelAtlas::create(StringUtils::format("%d",list.list.at(i).diamond),"shop/prop_num.png",21,28,'0');
+        money->setPosition(220+176*i,230);
         money->setAnchorPoint(Point::ANCHOR_MIDDLE_RIGHT);
         addChild(money);
         
-        auto yuan = Sprite::create("shop/diamond_icon_2.png");
-        yuan->setPosition(325+176*i,225);
-        yuan->setScale(0.6f);
-        addChild(yuan);
     }
 }
