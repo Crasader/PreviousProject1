704bf6be777be25c09b7c78db6e62ae2bd01c182
qz515880 <1195933889@qq.com>
2017-02-04 17:28:36 +0800

    上海麻将

--- Mahjong/Classes/mahjong/bill/BillInfo.cpp
+++ Mahjong/Classes/mahjong/bill/BillInfo.cpp
@@ -269,13 +269,13 @@ std::vector<BillContent> BillInfo::sortBillInfo(std::vector<BillContent> content
 
 void BillInfo::setShowPosition(){
     setIsPrivateBill(true);
-    getChildByTag(101)->setPositionX(890);
-    getChildByTag(102)->setPositionX(1230);
-    getChildByTag(104)->setPositionX(890);
-    getChildByTag(105)->setPositionX(525);
-    getChildByTag(2016)->setPositionX(605);
-    if(NULL != getChildByTag(1001))
-        getChildByTag(1001)->setPositionX(900);
+//    getChildByTag(101)->setPositionX(890);
+//    getChildByTag(102)->setPositionX(1230);
+//    getChildByTag(104)->setPositionX(890);
+//    getChildByTag(105)->setPositionX(525);
+//    getChildByTag(2016)->setPositionX(605);
+//    if(NULL != getChildByTag(1001))
+//        getChildByTag(1001)->setPositionX(900);
 }
 
 
