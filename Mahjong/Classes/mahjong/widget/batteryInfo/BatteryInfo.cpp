3316f4840d6f71bc871e554d5c6b0b1a58f31dd7
qz515880 <1195933889@qq.com>
2017-02-27 19:44:35 +0800

    上海麻将
    时间显示修改

--- Mahjong/Classes/mahjong/widget/batteryInfo/BatteryInfo.cpp
+++ Mahjong/Classes/mahjong/widget/batteryInfo/BatteryInfo.cpp
@@ -2,6 +2,31 @@
 #include "payment/android/CallAndroidMethod.h"
 #import "payment/ios/IOSBridge.h"
 
+
+std::string gettime()
+{
+    struct tm *ptm;
+    
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID || CC_TARGET_PLATFORM == CC_PLATFORM_IOS || CC_TARGET_PLATFORM == CC_PLATFORM_MAC)
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    ptm = localtime(&now.tv_sec);
+#endif
+    
+#if ( CC_TARGET_PLATFORM == CC_PLATFORM_WIN32 )
+    time_t timep;
+    time(&timep);
+    ptm = localtime(&timep);
+#endif
+    
+    // Format: 2015-08-19 17:51:36
+    char buffer[128] = { 0 };
+    sprintf(buffer, "%02d:%02d",
+            ptm->tm_hour, ptm->tm_min);
+    
+    return std::string(buffer);
+}
+
 bool BatteryInfo::init() {
 	Node::init();
 
@@ -20,7 +45,7 @@ bool BatteryInfo::init() {
 	timerBg->setPosition(90, _vSize.height-18);
 	addChild(timerBg);
 	
-	timerLabel = Label::createWithSystemFont("12:00","Arial",25);
+	timerLabel = Label::createWithSystemFont(gettime(),"Arial",25);
     timerLabel->setColor(Color3B(171,186,183));
 	timerLabel->setPosition(Vec2(timerBg->getContentSize().width / 2, timerBg->getContentSize().height / 2));  //24
 	timerBg->addChild(timerLabel);
@@ -30,36 +55,11 @@ bool BatteryInfo::init() {
     wifiInfo->setVisible(false);
 	addChild(wifiInfo);
 
-
+    updateInfo(0);
 	schedule(schedule_selector(BatteryInfo::updateInfo), 10.0f, CC_REPEAT_FOREVER, 0);
 	return true;
 }
 
-
-
-std::string gettime()
-{
-	struct tm *ptm;
-
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID || CC_TARGET_PLATFORM == CC_PLATFORM_IOS || CC_TARGET_PLATFORM == CC_PLATFORM_MAC)
-	struct timeval now;
-	gettimeofday(&now, NULL);
-	ptm = localtime(&now.tv_sec);
-#endif
-
-#if ( CC_TARGET_PLATFORM == CC_PLATFORM_WIN32 )
-	time_t timep;
-	time(&timep);
-	ptm = localtime(&timep);
-#endif
-
-	// Format: 2015-08-19 17:51:36
-	char buffer[128] = { 0 };
-	sprintf(buffer, "%02d:%02d",
-		ptm->tm_hour, ptm->tm_min);
-
-	return std::string(buffer);
-}
 
 
 void BatteryInfo::updateInfo(float delta)
