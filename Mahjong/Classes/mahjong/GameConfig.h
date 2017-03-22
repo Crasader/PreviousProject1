a4cf4e58a763fa7277b430ad209cc2e020227285
qiuzhong <1195933889@qq.com>
2017-03-22 14:33:46 +0800

    版本号添加

--- Mahjong/Classes/mahjong/GameConfig.h
+++ Mahjong/Classes/mahjong/GameConfig.h
@@ -9,7 +9,7 @@
 #ifndef GameConfig_h
 #define GameConfig_h
 
-#define PAY_PLAT_VALUE "14" //ios审核版本14，应用宝审核版本15.
+#define PAY_PLAT_VALUE "14" //ios审核版本16，应用宝审核版本17.
 
 //#define SERVER_ADDRESS "aliyun.5278-mobi.com"
 #define SERVER_ADDRESS "ceshi.5278-mobi.com"
