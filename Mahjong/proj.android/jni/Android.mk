317a0229171c4f1ebf403af3a0549c93cc6d3add
ganerzh <1195933889@qq.com>
2017-04-07 17:43:38 +0800

    移除youmi

--- Mahjong/proj.android/jni/Android.mk
+++ Mahjong/proj.android/jni/Android.mk
@@ -22,7 +22,6 @@ LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
 
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes \
-$(LOCAL_PATH)/../../yim/include \
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
@@ -30,13 +29,10 @@ $(LOCAL_PATH)/../../yim/include \
 
 LOCAL_STATIC_LIBRARIES := cocos2dx_static
 
-LOCAL_SHARED_LIBRARIES := YIM
-
 # _COCOS_LIB_ANDROID_BEGIN
 # _COCOS_LIB_ANDROID_END
 
 include $(BUILD_SHARED_LIBRARY)
-include $(LOCAL_PATH)/../../yim/lib/android/Android.mk
 
 $(call import-module,.)
 
