6937a4579e145b1f403b4ac6654a9fde773d4ae2
gamerzh <1195933889@qq.com>
2017-07-04 10:44:25 +0800

    红中麻将规则以及反馈

--- Mahjong/Classes/mahjong/common/setting/GameRuleDialog.cpp
+++ Mahjong/Classes/mahjong/common/setting/GameRuleDialog.cpp
@@ -28,84 +28,79 @@ bool GameRuleDialog::init(){
     closeMenu->setPosition(1000, 670);
     addChild(closeMenu);
     
-//    auto input_bg = Sprite::create("setting/text_bg.png");
-//    input_bg->setPosition(640, 320);
-//    addChild(input_bg);
-//    
-//    auto tabBg = Sprite::create("setting/tab_bg.png");
-//    tabBg->setPosition(470,610);
-//    addChild(tabBg);
-//    
-//    auto shnaghai = Sprite::create("setting/text_sh_1.png");
-//    shnaghai->setTag(1024);
-//    shnaghai->setPosition(390,610);
-//    addChild(shnaghai,2);
-//    
-//    auto hongzhong = Sprite::create("setting/text_hz_2.png");
-//    hongzhong->setTag(2024);
-//    hongzhong->setPosition(555,610);
-//    addChild(hongzhong,2);
-//    
-//    auto qun_normal = MenuItemImage::create("setting/tab_btn_1.png", "setting/tab_btn_1.png");
-//    auto qun_selected = MenuItemImage::create("setting/tab_btn_2.png", "setting/tab_btn_2.png");
-//    qunToggle = MenuItemToggle::createWithCallback(CC_CALLBACK_1(GameRuleDialog::showShangHai, this), qun_normal, qun_selected, NULL);
-//    qunToggle->setSelectedIndex(1);
-//    auto fangka_normal = MenuItemImage::create("setting/tab_btn_1.png", "setting/tab_btn_1.png");
-//    auto fangka_selected = MenuItemImage::create("setting/tab_btn_2.png", "setting/tab_btn_2.png");
-//    fangkaToggle = MenuItemToggle::createWithCallback(CC_CALLBACK_1(GameRuleDialog::showHongZhong, this), fangka_normal, fangka_selected, NULL);
-//    
-//    auto menu = Menu::create(qunToggle, fangkaToggle, NULL);
-//    menu->alignItemsHorizontallyWithPadding(0);
-//    menu->setPosition(470,610);
-//    addChild(menu,1);
-//    
-//    
-//    listView = ListView::create();
-//    listView->setDirection(ui::ScrollView::Direction::VERTICAL);//设置ListView布局方向
-//    listView->setTouchEnabled(true);//可触摸
-//    listView->setContentSize(Size(720,470));//设置ListView大小
-//    listView->ignoreContentAdaptWithSize(false);//开启锚点设置，false可更改锚点，true不可更改，Layer默认为point(0,0),其他Node为Point(0.5,0.5)
-//    listView->setAnchorPoint(Vec2(0.5, 0.5));//设置锚点，即锚点放在节点setPosition的位置，0，0：表示节点左下角；1，1：表示节点右上角；0.5，0.5表示节点中点
-//    listView->setPosition(Point(640,325));
-//    addChild(listView);
-//    
-//    for(auto var: getGameRulesShangHai()){
-//        Layout *customItem = Layout::create();
-//        customItem->setLayoutType(Layout::Type::ABSOLUTE);
-//        customItem->setContentSize(Size(720,40));
-//        listView->pushBackCustomItem(customItem);
-//        
-//        auto text = Label::createWithSystemFont(var,"arial",26);
-//        text->setColor(Color3B(255,255,255));
-//        text->setAnchorPoint(Point::ANCHOR_TOP_LEFT);
-//        text->setPosition(40,30);
-//        customItem->addChild(text);
-//    }
-//    
-//    
-//    
-//    listViewHz = ListView::create();
-//    listViewHz->setDirection(ui::ScrollView::Direction::VERTICAL);//设置ListView布局方向
-//    listViewHz->setTouchEnabled(true);//可触摸
-//    listViewHz->setContentSize(Size(720,470));//设置ListView大小
-//    listViewHz->ignoreContentAdaptWithSize(false);//开启锚点设置，false可更改锚点，true不可更改，Layer默认为point(0,0),其他Node为Point(0.5,0.5)
-//    listViewHz->setAnchorPoint(Vec2(0.5, 0.5));//设置锚点，即锚点放在节点setPosition的位置，0，0：表示节点左下角；1，1：表示节点右上角；0.5，0.5表示节点中点
-//    listViewHz->setPosition(Point(640,325));
-//    addChild(listViewHz);
-//    listViewHz->setVisible(false);
-//    
-//    for(auto var: getGameRulesHongZhong()){
-//        Layout *customItem = Layout::create();
-//        customItem->setLayoutType(Layout::Type::ABSOLUTE);
-//        customItem->setContentSize(Size(720,40));
-//        listViewHz->pushBackCustomItem(customItem);
-//        
-//        auto text = Label::createWithSystemFont(var,"arial",26);
-//        text->setColor(Color3B(255,255,255));
-//        text->setAnchorPoint(Point::ANCHOR_TOP_LEFT);
-//        text->setPosition(40,30);
-//        customItem->addChild(text);
-//    }
+    auto input_bg = Sprite::create("rule/content_bg.png");
+    input_bg->setPosition(640, 320);
+    addChild(input_bg);
+    
+ 
+    auto shnaghai = Sprite::create("rule/text_sh_1.png");
+    shnaghai->setTag(1024);
+    shnaghai->setPosition(390,590);
+    addChild(shnaghai,2);
+    
+    auto hongzhong = Sprite::create("rule/text_hz_2.png");
+    hongzhong->setTag(2024);
+    hongzhong->setPosition(555,590);
+    addChild(hongzhong,2);
+
+    auto qun_normal = MenuItemImage::create("rule/tab_btn_1.png", "rule/tab_btn_1.png");
+    auto qun_selected = MenuItemImage::create("rule/tab_btn_2.png", "rule/tab_btn_2.png");
+    qunToggle = MenuItemToggle::createWithCallback(CC_CALLBACK_1(GameRuleDialog::showShangHai, this), qun_normal, qun_selected, NULL);
+    
+    auto fangka_normal = MenuItemImage::create("rule/tab_btn_1.png", "rule/tab_btn_1.png");
+    auto fangka_selected = MenuItemImage::create("rule/tab_btn_2.png", "rule/tab_btn_2.png");
+    fangkaToggle = MenuItemToggle::createWithCallback(CC_CALLBACK_1(GameRuleDialog::showHongZhong, this), fangka_normal, fangka_selected, NULL);
+    fangkaToggle->setSelectedIndex(1);
+    auto menu = Menu::create(qunToggle, fangkaToggle, NULL);
+    menu->alignItemsHorizontallyWithPadding(0);
+    menu->setPosition(465,596);
+    addChild(menu,1);
+    
+    
+    listView = ListView::create();
+    listView->setDirection(ui::ScrollView::Direction::VERTICAL);//设置ListView布局方向
+    listView->setTouchEnabled(true);//可触摸
+    listView->setContentSize(Size(720,470));//设置ListView大小
+    listView->ignoreContentAdaptWithSize(false);//开启锚点设置，false可更改锚点，true不可更改，Layer默认为point(0,0),其他Node为Point(0.5,0.5)
+    listView->setAnchorPoint(Vec2(0.5, 0.5));//设置锚点，即锚点放在节点setPosition的位置，0，0：表示节点左下角；1，1：表示节点右上角；0.5，0.5表示节点中点
+    listView->setPosition(Point(640,325));
+    addChild(listView);
+    
+    for(auto var: getGameRulesShangHai()){
+        Layout *customItem = Layout::create();
+        customItem->setLayoutType(Layout::Type::ABSOLUTE);
+        customItem->setContentSize(Size(720,40));
+        listView->pushBackCustomItem(customItem);
+        
+        auto text = Label::createWithSystemFont(var,"arial",26);
+        text->setColor(Color3B(177,121,33));
+        text->setAnchorPoint(Point::ANCHOR_TOP_LEFT);
+        text->setPosition(40,30);
+        customItem->addChild(text);
+    }
+    
+    listViewHz = ListView::create();
+    listViewHz->setDirection(ui::ScrollView::Direction::VERTICAL);//设置ListView布局方向
+    listViewHz->setTouchEnabled(true);//可触摸
+    listViewHz->setContentSize(Size(720,470));//设置ListView大小
+    listViewHz->ignoreContentAdaptWithSize(false);//开启锚点设置，false可更改锚点，true不可更改，Layer默认为point(0,0),其他Node为Point(0.5,0.5)
+    listViewHz->setAnchorPoint(Vec2(0.5, 0.5));//设置锚点，即锚点放在节点setPosition的位置，0，0：表示节点左下角；1，1：表示节点右上角；0.5，0.5表示节点中点
+    listViewHz->setPosition(Point(640,325));
+    addChild(listViewHz);
+    listViewHz->setVisible(false);
+    
+    for(auto var: getGameRulesHongZhong()){
+        Layout *customItem = Layout::create();
+        customItem->setLayoutType(Layout::Type::ABSOLUTE);
+        customItem->setContentSize(Size(720,40));
+        listViewHz->pushBackCustomItem(customItem);
+        
+        auto text = Label::createWithSystemFont(var,"arial",26);
+        text->setColor(Color3B(177,121,33));
+        text->setAnchorPoint(Point::ANCHOR_TOP_LEFT);
+        text->setPosition(40,30);
+        customItem->addChild(text);
+    }
     
     return true;
 }
@@ -117,26 +112,26 @@ void GameRuleDialog::closeView(){
 
 
 void GameRuleDialog::showShangHai(Ref* ref){
-    qunToggle->setSelectedIndex(1);
-    fangkaToggle->setSelectedIndex(0);
+    qunToggle->setSelectedIndex(0);
+    fangkaToggle->setSelectedIndex(1);
     if(NULL != getChildByTag(1024)){
-        ((Sprite*)getChildByTag(1024))->setTexture("setting/text_sh_1.png");
+        ((Sprite*)getChildByTag(1024))->setTexture("rule/text_sh_1.png");
     }
     if(NULL != getChildByTag(2024)){
-        ((Sprite*)getChildByTag(2024))->setTexture("setting/text_hz_2.png");
+        ((Sprite*)getChildByTag(2024))->setTexture("rule/text_hz_2.png");
     }
     listView->setVisible(true);
     listViewHz->setVisible(false);
 }
 
 void GameRuleDialog::showHongZhong(Ref* ref){
-    qunToggle->setSelectedIndex(0);
-    fangkaToggle->setSelectedIndex(1);
+    qunToggle->setSelectedIndex(1);
+    fangkaToggle->setSelectedIndex(0);
     if(NULL != getChildByTag(1024)){
-        ((Sprite*)getChildByTag(1024))->setTexture("setting/text_sh_2.png");
+        ((Sprite*)getChildByTag(1024))->setTexture("rule/text_sh_2.png");
     }
     if(NULL != getChildByTag(2024)){
-        ((Sprite*)getChildByTag(2024))->setTexture("setting/text_hz_1.png");
+        ((Sprite*)getChildByTag(2024))->setTexture("rule/text_hz_1.png");
     }
     listView->setVisible(false);
     listViewHz->setVisible(true);
