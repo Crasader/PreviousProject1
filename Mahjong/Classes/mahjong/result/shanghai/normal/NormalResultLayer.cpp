0132ef090ad96168bc09c31887cf310c6311be4a
gamerzh <1195933889@qq.com>
2017-07-12 18:01:35 +0800

    提交

--- Mahjong/Classes/mahjong/result/shanghai/normal/NormalResultLayer.cpp
+++ Mahjong/Classes/mahjong/result/shanghai/normal/NormalResultLayer.cpp
@@ -148,7 +148,7 @@ void NormalResultLayer::showRoomInfo(){
         if(GAMEDATA::getInstance()->getPrivateEmsc() == "1"){
             emsc->setVisible(true);
         }
-        auto jucount = Sprite::create();
+        auto jucount = Sprite::create("");
         addChild(jucount);
         
         if(GAMEDATA::getInstance()->getFriendOpenRoomResp().prjushu == "4"){
